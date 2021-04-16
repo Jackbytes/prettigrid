afa30e41862bbcf0e8e8435f18a864137af4e163
Jack <jackjharrington@icloud.com>
2021-04-16 22:57:07 +0100

    Basic layout of class.

---
+++ prettigrid/prettigrid.py
@@ -0,0 +1,26 @@
+from PIL import Image, ImageDraw, ImageColor, ImageFont
+import glob
+
+class pretti(self):
+
+    def __init__(self):
+
+        pass
+    
+    def update(config,self):
+
+        pass
+
+    def new_rulebook(self,rulebook):
+
+        if callable(rulebook):
+            
+            pass
+
+        elif type(rulebook) == dict:
+
+            pass
+
+        else:
+
+            raise TypeError("Rulebook must be dictionary or function.")
